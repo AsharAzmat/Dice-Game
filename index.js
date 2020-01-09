6e654e7b31b5b3ae1d2c53027161810bd87f9e4c
AsharAzmat <45441801+AsharAzmat@users.noreply.github.com>
2020-01-09 16:15:55 -0500

    Add Dice Game files

---
+++ index.js
@@ -0,0 +1,25 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;    //1-6
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;    //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";    //dice1.png - dice6.png
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";    //dice1.png - dice6.png
+
+
+var randomImageSource = "images/" + randomDiceImage;      //images/dice1.png - images/dice6.png
+var randomImageSource2 = "images/" + randomDiceImage2;      //images/dice1.png - images/dice6.png
+
+
+document.querySelector(".img1").setAttribute("src", randomImageSource);
+document.querySelector(".img2").setAttribute("src", randomImageSource2);
+
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML="🚩Player 1 Wins!";
+}
+else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML="Player 2 Wins!🚩";
+}
+else {
+  document.querySelector("h1").innerHTML="Draw!";
+}
